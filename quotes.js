5c6cb3d36461afd44ffd8bf0a0d7658b3a0221db
Leo Yockey <leoyockey@gmail.com>
2019-10-09 19:34:03 -0700

    initial api setup

---
+++ quotes.js
@@ -0,0 +1,8 @@
+module.exports = quotes = [
+    'Making it through a full 24 hours without a single misstep is called “Reaganing”',
+    'Human empathy is as useless as the Winter Olympics',
+    'Have you ever considered becoming the celebrity face of the Republican Party?',
+    'I\'m not a creative type like you, with your work sneakers and left-handedness.',
+    'Factories provide three things this country desperately needs: jobs, pride, and material for Bruce Springsteen songs.',
+    'I like you. You have the boldness of a much younger woman.'
+];
