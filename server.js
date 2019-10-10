3364877aaedab0d113a6ba6ff189412145d579aa
ljyockey <leoyockey@gmail.com>
2019-10-09 20:35:23 -0700

    allow port to be set as env variable

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const quotes = require('./quotes');
 
 const app = express();
-const port = 8080
+const port = process.env.PORT || 8080;
 
 app.all('*', (req, res, next) => {
-    const timestamp = new Date().getTime();
-    console.log(`${timestamp} - ${req.method} request for ${req.path}`);
+  const timestamp = new Date().getTime();
+  console.log(`${timestamp} - ${req.method} request for ${req.path}`);
 
-    res.set('Access-Control-Allow-Origin', '*');
-    next();
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
 });
 
 app.get('/quotes', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * quotes.length)
+  const randomIndex = Math.floor(Math.random() * quotes.length);
   res
     .status(200)
-    .send([quotes[randomIndex]])
-})
+    .send([quotes[randomIndex]]);
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
